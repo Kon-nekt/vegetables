809027a8a3d48cf7a8ee91550d43544a911bef34
Kon-nekt <redexe141@gmail.com>
2020-03-29 22:07:39 +0300

    Update slide.js

--- src/Components/slide.js
+++ src/Components/slide.js
@@ -8,7 +8,7 @@ import img from './1.jpg'
 const DemoCarousel = () =>
     {
         return (
-            <Carousel showThumbs = {false} autoPlay={true} infiniteLoop={true} dynamicHeight={false} showStatus={false}> showIndicators={false} interval={10000}
+            <Carousel showThumbs = {false} autoPlay={true} infiniteLoop={true} dynamicHeight={false} showStatus={false} showIndicators={false} interval={10000}>
                 <div>
                     <img alt="" src={img} />
                     <p className="legend">Legend 1</p>
