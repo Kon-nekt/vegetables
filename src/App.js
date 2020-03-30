43e7d9f5adc3c75ad8331ed959c75e3e08ba6f46
Kon-nekt <redexe141@gmail.com>
2020-03-30 23:30:56 +0300

    ыаыаыа

--- src/App.js
+++ src/App.js
@@ -4,21 +4,45 @@ import DemoCarousel from './Components/slide'
 import Advantages from './Components/advantages';
 import Sales from './Components/sales';
 import Categories from './Components/categories';
-import Footer from './Components/footer'
+import Footer from './Components/footer';
+import Header from './Components/header';
+import {Element,scroller } from 'react-scroll'
+import HeaderPC from './Components/headerPC';
 
-function App() {
-  return (
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={visibility:false};
+    this.handleClick=this.handleClick.bind(this);
+  }
+  handleClick(link){
+    scroller.scrollTo(`myScrollToElement${link}`, {
+        offset: -60,
+        duration: 300,
+        smooth: "easeInOutQuint"
+    });
+  }
+
+  render() {
+    return (
     <div className="App">
+      <HeaderPC anchor={this.handleClick}/>
+      <Header anchor={this.handleClick}/>
       <DemoCarousel/>
       <Advantages/>
       <Sales />
+      <Element name="myScrollToElement1">
       <Categories name="Новинки" />
+      </Element>
+      <Element name="myScrollToElement2">
       <Categories name="Овощи" />
+      </Element>
       <Categories name="Фрукты" />
       <Categories name="Грибы" />
       <Footer />
     </div>
   );
+  }  
 }
 
 export default App;
