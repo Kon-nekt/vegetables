16a3ba04572369e2c7200475eb79615a5cad85cf
Kon-nekt <redexe141@gmail.com>
2020-04-03 21:22:09 +0300

    dadadad

--- src/Components/header.js
+++ src/Components/header.js
@@ -5,22 +5,25 @@ import '../css/header.css'
 class Header extends React.Component {
     constructor(props) {
     super(props);
-    this.state={onActiveFirst:'Select',onActiveSecond:'nonSelect',onActiveThird:'nonSelect'};
+    this.state={onActiveFirst:'Select',onActiveSecond:'nonSelect',onActiveThird:'nonSelect', menuOpen:false};
   }
-  handleMenuClick(event) {
-    event.preventDefault();
-    // Using the parent component's state to keep track of the menu
-    this.setState({menuOpen: false});
+
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})  
+  }
+
+  closeMenu () {
+    this.setState({menuOpen: false})
   }
   render () {
     // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
     return (
-      <Menu isOpen={ false }>
-        <a id="vegetables" className="menu-item" onClick={()=>this.props.anchor('Овощи')}><div>Овощи</div></a>
-        <a id="fruits" className="menu-item" onClick={()=>this.props.anchor('Фрукты')}><div>Фрукты</div></a>
-        <a id="nuts" className="menu-item" onClick={()=>this.props.anchor('Орехи')}><div>Орехи</div></a>
-        <a id="candies" className="menu-item" onClick={()=>this.props.anchor('Конфеты')}><div>Конфеты</div></a>
-        <a id="chocolate" className="menu-item" onClick={()=>this.props.anchor('Фрукты в шоколаде')}><div>Фрукты в шоколаде</div></a>
+      <Menu isOpen={this.state.menuOpen} onStateChange={(state) => this.handleStateChange(state)}>
+        <a id="vegetables" className="menu-item" onClick={()=>{this.props.anchor('Овощи');this.closeMenu();}}><div>Овощи</div></a>
+        <a id="fruits" className="menu-item" onClick={()=>{this.props.anchor('Фрукты');this.closeMenu();}} ><div>Фрукты</div></a>
+        <a id="nuts" className="menu-item" onClick={()=>{this.props.anchor('Орехи');this.closeMenu();}} ><div>Орехи</div></a>
+        <a id="candies" className="menu-item" onClick={()=>{this.props.anchor('Конфеты');this.closeMenu();}} ><div>Конфеты</div></a>
+        <a id="chocolate" className="menu-item" onClick={()=>{this.props.anchor('Фрукты в шоколаде');this.closeMenu();}}><div>Фрукты в шоколаде</div></a>
       </Menu>
     );
   }
